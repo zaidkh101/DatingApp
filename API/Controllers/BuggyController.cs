1a36e3cd1d6c9ca4380ef0d883f4e695d8ff7b0d
Zaid Al-Khader <zeidkh1998@gmail.com>
2021-08-07 23:43:57 +0300

    Gloabl Error Hangdling | Zaid

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,61 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecert()
+        {
+            return "Secert Text";
+        }
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+
+            if (thing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(thing);
+
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+             var thing = _context.Users.Find(-1);
+
+                var thingToReturn = thing.ToString();
+
+                return thingToReturn;
+          
+
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("this was not a good request");
+        }
+
+    }
+}
